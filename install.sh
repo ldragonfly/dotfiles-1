373c52d92ec6d71b6028d879e262028b9b611e6e
seock <intto2008@gmail.com>
2018-01-26 03:34:23 +0900

    Install pyenv
    Now `install.sh` installs pyenv. It installs pyenv, python build
    dependencies, and separated neovim virtual environment.

--- install.sh
+++ install.sh
@@ -18,6 +18,23 @@ if [ ! -d dircolors-solarized ]; then
     echo "eval \$(dircolors -b \$HOME/package/dircolors-solarized/dircolors.ansi-dark)\n" >> $HOME/.zshrc
 fi
 
+# install pyenv
+if [ ! -d $HOME/.pyenv ]; then
+    # https://github.com/pyenv/pyenv-installer#github-way-recommended
+    curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+
+    # https://github.com/pyenv/pyenv/wiki/Common-build-problems#requirements
+    if [ -x $(command -v apt) ]; then
+        sudo apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev \
+        libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+        xz-utils tk-dev
+    # https://aur.archlinux.org/packages/python-git#pkgdeps
+    elif [ -x $(command -v pacman) ]; then
+        sudo pacman -S bzip2 expat gdbm libffi openssl zlib bluez-libs git sqlite \
+        valgrind xz tk libtirpc
+    fi
+fi
+
 # install neovim
 if [ ! $(command -v nvim) ]; then
     echo "installing neovim.."
@@ -54,4 +71,21 @@ if [ -n "$INSTALL_TMUX_GIT" ]; then
 
     git clone https://github.com/tmux/tmux tmux
     sh -c "cd tmux; ./configure; make -j$MAX_THREAD_COUNT; sudo make install"
+fi
+
+PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+
+if [ ! -d $HOME/.pyenv/versions/neovim2 ]; then
+    pyenv install 2.7.14 --verbose
+    pyenv virtualenv 2.7.14 neovim2
+    pyenv activate neovim2
+    pip install neovim
+fi
+
+if [ ! -d $HOME/.pyenv/versions/neovim3 ]; then
+    pyenv install 3.6.4 --verbose
+    pyenv virtualenv 3.6.4 neovim3
+    pyenv activate neovim3
+    pip install neovim
 fi
