014cf0bb4d4cb476fc2b0677f45a794183b02f45
didrod <intto2008@gmail.com>
2018-01-26 05:27:18 +0900

    Install nvim plugins automatically

--- install.sh
+++ install.sh
@@ -99,3 +99,4 @@ if [ ! -f $HOME/.local/share/nvim/site/autoload/plug.vim ]; then
     curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+nvim +:PlugInstall
